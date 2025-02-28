91885b2330cb38aa297a9e6e87a978b9913ed5e1
Gavin Finn <gavinfinn3@hotmail.com>
2025-02-27 16:41:56 -0800

    appointment-planner: implemented ContactsPage

--- Projects/appointment-planner/src/containers/contactsPage/ContactsPage.js
+++ Projects/appointment-planner/src/containers/contactsPage/ContactsPage.js
@@ -3,11 +3,15 @@ import React, { useState, useEffect } from "react";
 import { ContactForm } from "../../components/contactForm/ContactForm";
 import { TileList } from "../../components/tileList/TileList";
 
-export const ContactsPage = () => {
+export const ContactsPage = ({ contacts, addContact }) => {
   /*
   Define state variables for 
   contact info and duplicate check
   */
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [duplicate, setDuplicate] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -15,17 +19,47 @@ export const ContactsPage = () => {
     Add contact info and clear data
     if the contact name is not a duplicate
     */
+    if (duplicate !== false) {
+      addContact(name, phone, email)
+      setName("");
+      setPhone("");
+      setEmail("");
+    }
   };
 
   /*
   Using hooks, check for contact name in the 
   contacts array variable in props
   */
+  useEffect(() => {
+    const checkDuplicate = () => {
+      const nameFound = contacts.find((contact) => contact.name === name);
+      if (nameFound !== undefined) {
+        return true;
+      }
+      return false;
+    }
+
+    if (checkDuplicate()) {
+      setDuplicate(true);
+    } else {
+      setDuplicate(false);
+    }
+  }, [name, contacts, duplicate])
 
   return (
     <div>
       <section>
         <h2>Add Contact</h2> 
+        <ContactForm 
+          name={name}
+          setName={setName}
+          phone={phone}
+          setPhone={setPhone}
+          email={email}
+          setEmail={setEmail}
+          handleSubmit={handleSubmit}
+        />
       </section>
       <hr />
       <section>
