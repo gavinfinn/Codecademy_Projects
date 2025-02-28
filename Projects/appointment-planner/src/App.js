bac8443b83fd68d02665c7ee222ed503cc8da76c
Gavin Finn <gavinfinn3@hotmail.com>
2025-02-27 16:16:35 -0800

    appointment-planner: implemented variables to track contacts and callback functions for contacts and appointments

--- Projects/appointment-planner/src/App.js
+++ Projects/appointment-planner/src/App.js
@@ -9,17 +9,41 @@ function App() {
   Define state variables for 
   contacts and appointments 
   */
+  const [contacts, setContacts] = useState([]);
+  const [appointments, setAppointments] = useState([]);  
 
   /*
   Implement functions to add data to
   contacts and appointments
   */
+  const addContact = (name, phone, email) => {
+    setContacts([
+                ...contacts,
+                {
+                  name: name,
+                  phone: phone,
+                  email: email
+                } 
+              ]);
+  };
+
+  const addAppointment = (name, contact, date, time) => {
+    setAppointments([
+              ...appointments,
+              {
+                name: name,
+                contact: contact, 
+                date: date,
+                time: time
+              }
+    ]);
+  };
 
   const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={ <Root/> }>
       <Route index element={ <Navigate to={ROUTES.CONTACTS} replace/> }/>
-      <Route path={ROUTES.CONTACTS} element={ <ContactsPage /> /* Add props to ContactsPage */ }/>
-      <Route path={ROUTES.APPOINTMENTS} element={ <AppointmentsPage /> /* Add props to AppointmentsPage */ }/>
+      <Route path={ROUTES.CONTACTS} element={ <ContactsPage contacts={contacts} addContact={addContact} /> /* Add props to ContactsPage */ }/>
+      <Route path={ROUTES.APPOINTMENTS} element={ <AppointmentsPage appointments={appointments} addAppointment={addAppointment} /> /* Add props to AppointmentsPage */ }/>
     </Route>
   ));
   
